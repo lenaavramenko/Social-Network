030512c1751119a45dce198f30e96c69b2ba9bc9
Jelena Avramenko <jelena.avramenko@accenture.com>
2019-06-06 17:18:42 +0300

    vuex

--- src/main/java/accenture/adf/SN/config/WebSecurityConfig.java
+++ src/main/java/accenture/adf/SN/config/WebSecurityConfig.java
@@ -28,7 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/", "/login**", "/js/**", "/error**").permitAll()
                 .anyRequest().authenticated()
-                .and().formLogin().loginPage("/login_a").permitAll()
+                .and().formLogin().loginPage("/login").permitAll()
                 .and().logout().logoutSuccessUrl("/").permitAll()
                 .and()
                 .csrf().disable();
